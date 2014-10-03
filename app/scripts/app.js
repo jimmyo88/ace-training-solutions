84ae11b212ee100cd1a60f2ee9c31ac88c9c3cea
james.owen <james.owen@autotrader.co.uk>
2014-10-04 00:19:09 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aceTrainingApp
+ * @description
+ * # aceTrainingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aceTrainingApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'HomeController'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }).directive('disableAnimation', function($animate){
+        return {
+            restrict: 'A',
+            link: function($scope, $element, $attrs){
+                $attrs.$observe('disableAnimation', function(value){
+                    $animate.enabled(!value, $element);
+                });
+            }
+        };
+    });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aceTrainingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aceTrainingApp
+ */
+angular.module('aceTrainingApp').controller('HomeController', function ($scope) {
+    $scope.myInterval = 7000;
+    $scope.slides = [
+        {image: 'images/home-stock-image-1.jpg'},
+        {image: 'images/home-stock-image-2.jpg'},
+        {image: 'images/home-stock-image-3.jpg'}];
+    $scope.information = [
+        {
+         title: 'Customer Service',
+         image: 'images/customer-service-stock.jpg',
+         summary: 'Deliver outstanding service through improved knowledge and skills.'
+        },
+        {
+            title: 'Increasing Revenue',
+            image: 'images/increasing-revenue-stock.jpg',
+            summary: 'summary: Generate additional revenue by developing individual and team selling skills.'
+
+        },
+        {
+            title: 'Team Leadership',
+            image: 'images/leadership-stock.png',
+            summary: 'Motivate and inspire others to offer outstanding service.'
+        }
+    ];
+});
