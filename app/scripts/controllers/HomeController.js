590a5635fa2bcf077a05a4faa9a8842783be6544
james.owen <james.owen@autotrader.co.uk>
2014-11-10 13:34:12 +0000

    removed all images from app - images now hosted on S3

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -3,24 +3,24 @@
 angular.module('aceTrainingApp').controller('HomeController', function ($scope) {
     $scope.myInterval = 4000;
     $scope.slides = [
-        {image: 'images/home-stock-image-1.jpg'},
-        {image: 'images/home-stock-image-2.jpg'},
-        {image: 'images/home-stock-image-3.jpg'}];
+        {image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/home/home-stock-image-1.jpg'},
+        {image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/home/home-stock-image-2.jpg'},
+        {image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/home/home-stock-image-3.jpg'}];
     $scope.information = [
         {
          title: 'Customer Service',
-         image: 'images/customer-service-stock.jpg',
+         image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/home/customer-service-stock.jpg',
          summary: 'Deliver outstanding service through improved knowledge and skills.'
         },
         {
             title: 'Increasing Revenue',
-            image: 'images/increasing-revenue-stock.jpg',
+            image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/home/increasing-revenue-stock.jpg',
             summary: 'summary: Generate additional revenue by developing individual and team selling skills.'
 
         },
         {
             title: 'Team Leadership',
-            image: 'images/leadership-stock.png',
+            image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/home/leadership-stock.png',
             summary: 'Motivate and inspire others to offer outstanding service.'
         }
     ];
--- app/scripts/controllers/ProductsController.js
+++ app/scripts/controllers/ProductsController.js
@@ -5,34 +5,34 @@
         $scope.products = [
             {
                 title: 'Mentoring schemes',
-                image: 'images/product-mentoring-image.jpg',
+                image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/product/product-mentoring-image.jpg',
                 summary: 'One on one tuition ensuring you are at your very best all the time.'
             },
             {
                 title: 'In-house training',
-                image: 'images/product-inhouse-image.jpg',
+                image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/product/product-inhouse-image.jpg',
                 summary: 'Schemes designed for groups of employees. Build better teams quickly.'
 
             },
             {
                 title: 'Lectures & Seminars',
-                image: 'images/product-lecture-image.jpg',
+                image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/product/product-lecture-image.jpg',
                 summary: 'The very best in professional education. Our facilities are among the best.'
             },
             {
                 title: 'Team development',
-                image: 'images/product-teamdevelopment-image.jpg',
+                image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/product/product-teamdevelopment-image.jpg',
                 summary: 'Develop teams to increase efficiency and productivity in a fun environment.'
             },
             {
                 title: 'Leadership',
-                image: 'images/product-leadership-image.jpg',
+                image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/product/product-leadership-image.jpg',
                 summary: 'Learn how to inspire those around you and how to take ownership.'
 
             },
             {
                 title: 'Morale boosting',
-                image: 'images/product-morale-image.jpg',
+                image: 'https://s3-eu-west-1.amazonaws.com/ace-training-services/product/product-morale-image.jpg',
                 summary: 'Motivate and inspire others to offer outstanding service.'
             }
         ];
